aa5e9d41ca61dd58575296abe9c5b57e4f5773e0
Landen English <englishlanden@gmail.com>
2024-09-02 19:30:21 -0400

    Fix flicker

--- pages/index.vue
+++ pages/index.vue
@@ -46,39 +46,41 @@ const handleSubmit = () => {
 </script>
 
 <template>
-  <div class="flex h-full flex-col justify-between p-4">
-    <div
-      class="flex flex-col space-y-4 overflow-auto"
-      style="max-height: calc(100vh - 120px)"
-    >
+  <ClientOnly>
+    <div class="flex h-full flex-col justify-between p-4">
       <div
-        v-for="(chat, index) in chatHistory"
-        :key="index"
-        :class="[
-          'max-w-xs rounded-lg p-4',
-          chat.isUser ? 'ml-auto text-right' : 'mr-auto text-left',
-        ]"
+        class="flex flex-col space-y-4 overflow-auto"
+        style="max-height: calc(100vh - 120px)"
       >
-        <div>{{ chat.message }}</div>
-        <div class="mt-1 text-xs text-gray-500">
-          {{ formatDistanceToNow(chat.writtenAt, { addSuffix: true }) }}
+        <div
+          v-for="(chat, index) in chatHistory"
+          :key="index"
+          :class="[
+            'max-w-xs rounded-lg p-4',
+            chat.isUser ? 'ml-auto text-right' : 'mr-auto text-left',
+          ]"
+        >
+          <div>{{ chat.message }}</div>
+          <div class="mt-1 text-xs text-gray-500">
+            {{ formatDistanceToNow(chat.writtenAt, { addSuffix: true }) }}
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="flex items-center space-x-4">
-      <InputText
-        v-model="prompt"
-        placeholder="Enter a prompt"
-        class="flex-grow rounded border px-4 py-2 focus:outline-none focus:ring-2"
-        @keydown.enter="handleSubmit"
-      />
-      <Button
-        :disabled="!prompt"
-        class="rounded py-2 font-semibold"
-        icon="pi pi-send"
-        @click="handleSubmit"
-      />
+      <div class="flex items-center space-x-4">
+        <InputText
+          v-model="prompt"
+          placeholder="Enter a prompt"
+          class="flex-grow rounded border px-4 py-2 focus:outline-none focus:ring-2"
+          @keydown.enter="handleSubmit"
+        />
+        <Button
+          :disabled="!prompt"
+          class="rounded py-2 font-semibold"
+          icon="pi pi-send"
+          @click="handleSubmit"
+        />
+      </div>
     </div>
-  </div>
+  </ClientOnly>
 </template>
